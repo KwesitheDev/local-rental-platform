3393903b174ca67d795b3280c7b950c1cf3a2b15
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-05 03:00:43 +0100

    listings model

---
+++ backend/models/Listing.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+    title:{ type: String, required: true },
+    description: { type: String, },
+    type:{ type: String,enum: ['housing', 'equipment', 'vehicle'] , required: true },
+    pricePerDay: { type: Number, required: true },
+    availableFrom: { type: Date, required: true },
+    availableTo: { type: Date, required: true },
+    category: { type: String },
+    location: { type: String },
+    images: { type: [String] },
+    owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
+    status: { type: String, enum: ['available', 'booked', 'unlisted'], default: 'available' }
+
+})
+
+listingSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Listing', listingSchema)
