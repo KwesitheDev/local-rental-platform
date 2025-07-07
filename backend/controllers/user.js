83da5a53161b033e4c52efd87005f468c237c8a6
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-07 15:04:59 +0100

    user.js

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -1,28 +1,32 @@
 const usersRouter = require('express').Router()
 const User = require('../models/User')
 const bcrypt = require('bcrypt')
+const User = require('../models/user');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const config = require('../utils/config');
 
 
-usersRouter.post('/', async (req, res) => {
+usersRouter.post('/register', async (req, res) => {
     const { name, email, password, role } = req.body
 
-    //check for username and password not missing
-    if (!username || !password) {
+    //check for email and password not missing
+    if (!email || !password) {
         return res
                 .status(400)
-                .json({ error: 'username and password are required' })
+                .json({ error: 'email and password are required' })
 
     }
 
-    //Check for length of username & password
-    if (username.length < 3 || password.length < 3) {
-        return res.status(400).json({ error: 'username and password must be at least 3 characters long' })
+    //Check for length of useremail & password
+    if (email.length < 3 || password.length < 3) {
+        return res.status(400).json({ error: 'email and password must be at least 3 characters long' })
     }
 
-    //Check for unique username
-    const existingUser = await User.findOne({ username })
+    //Check for unique email
+    const existingUser = await User.findOne({ email })
     if (existingUser) {
-        return res.status(400).json({ error: 'username must be unique' })
+        return res.status(400).json({ error: 'email must be unique' })
     }
 
     const saltRounds = 10
