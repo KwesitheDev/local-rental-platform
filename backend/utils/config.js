fc6f9d9d6e66ef8ebc15e581996b69676c23861f
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-10 03:14:47 +0100

    import Screat string to config

--- backend/utils/config.js
+++ backend/utils/config.js
@@ -2,7 +2,8 @@ require('dotenv').config()
 
 const MONGODB_URI = process.env.MONGODB_URI
 const PORT = process.env.PORT
+const SECRET = process.env.SECRET
 
 module.exports = {
-    MONGODB_URI,PORT
+    MONGODB_URI,PORT,SECRET
 }
