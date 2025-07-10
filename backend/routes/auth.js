632707dc26929b66ffd9aba9ca14e92782c5d277
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-11 00:54:49 +0100

    register route added

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -1,7 +1,7 @@
 const authRouter = express.Router()
-const {  login } = require('../controllers/authController')
+const {  login, register } = require('../controllers/authController')
 
-//authRouter.post('/register', register)
+authRouter.post('/register', register)
 authRouter.post('/login', login)
 
 module.exports = authRouter
