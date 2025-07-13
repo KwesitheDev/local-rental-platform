4bf116cc1b452a304eb18092d11dba09e2c8dafa
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-13 03:05:56 +0100

    cancelBooking, getBookingById, getUserBookings, updateBookingStatus

--- backend/controllers/bookingController.js
+++ backend/controllers/bookingController.js
@@ -24,3 +24,71 @@ const createBooking = async (req, res) => {
         return res.status(500).json({error: 'Failed to create booking'})
     }
 }
+
+
+const cancelBooking = async (req, res) => {
+    try {
+        const bookingId = req.params.id
+        const booking = await Booking.findById(bookingId)
+
+        if (!booking) return res.status(404).json({error: 'Booking not found'})
+        if (!booking.renter.equals(renter.user.id))return res.status(403).json({error: 'Unauthorized Action'})
+            
+        booking.status='cancelled'   
+        await booking.save()
+
+        res.json({message: 'Booking cancelled', booking})
+    }catch(error){
+        res.status(500).json({error: 'Failed to cancel booking'})
+    }
+}
+
+const getUserBookings = async (req, res) => {
+    try {
+        const bookings = await Booking.find({renter: renter.user.id}).populate('listing')
+        res.json(bookings)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch bookings' })
+    }
+}
+
+const getBookingById = async (req, res) => {
+    try {
+        const { id } = req.params
+        const booking = await Booking.findById(id).populate('listing renter')
+
+        if (!booking) return res.status(404).json({ error: 'Booking not found' })
+        res.json(booking)
+    } catch (error) {
+        res.status(500).json({ error: 'Failed to fetch booking' })
+    }
+}
+
+
+const updateBookingStatus = async (req,res) =>{
+    try {
+        const {id} = req.params
+        const { status } = req.body
+        
+        if (!['pending', 'confirmed', 'rejected', 'cancelled'].includes(status)) {
+            return res.status(400).json({
+                error: 'Invalid status value'
+            })
+        }
+
+        const updated = await Booking.findByIdAndUpdate(id, {status}, {new: true})
+        res.json(updated)
+    } catch (error) {
+        res.status(500).json({
+            error:'Failed to update booking status'
+        })
+    }
+}
+
+module.exports ={
+    getBookingById,
+    createBooking,
+    getUserBookings,
+    updateBookingStatus,
+    cancelBooking
+}
