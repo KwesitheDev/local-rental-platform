d76ff92ca79c20111d8887190b71c712c6e4dada
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-18 05:34:47 +0100

    middleware

---
+++ backend/middleware/auth.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+import config from ("../utils/config");
+
+const requireAuth = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+  if (!authHeader || !authHeader.toLowerCase().startsWith("bearer ")) {
+    return res.status(401).json({ error: "Missing or invalid token" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, config.SECRET); c
+    req.user = decoded;
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Token invalid or expired" });
+  }
+}
+
+export default { requireAuth };
