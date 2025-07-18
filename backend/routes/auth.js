ecc28968524347e6c9238e01b7c70817e093aeb9
kwesitheDev <174883738+KwesitheDev@users.noreply.github.com>
2025-07-18 04:26:46 +0100

    project restart

--- backend/routes/auth.js
+++
@@ -1,7 +0,0 @@
-const authRouter = express.Router()
-const {  login, register } = require('../controllers/authController')
-
-authRouter.post('/register', register)
-authRouter.post('/login', login)
-
-module.exports = authRouter
--- backend/utils/logger.js
+++
@@ -1,5 +0,0 @@
-const error = (...params)=> console.error(...params)
-const info = (...params)=> console.log(...params)
-module.exports={
-    error,info
-}
